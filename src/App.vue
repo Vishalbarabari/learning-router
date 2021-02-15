35296d59cdefb8f4a650087296c6f4b4f5d094e6
vishalbarabari <vishalbarabari@gmail.com>
2021-02-15 10:19:58 +0530

    travel app using router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <thenavigation/>
+    <router-view  :key="$route.path"/>
   </div>
 </template>
 
+<script>
+import thenavigation from "@/components/thenavigation"
+export default{
+  components:{
+    thenavigation
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -24,9 +29,10 @@
 #nav a {
   font-weight: bold;
   color: #2c3e50;
+  padding: 0 10px;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #dd402b;
 }
 </style>
---
+++ src/components/thenavigation.vue
@@ -0,0 +1,68 @@
+<template>
+    <nav id="nav">
+        <p class="logo">THIS IS A TRAVEL WEBSITE</p>
+        <ul class="nav-links">
+            <li class="links">
+                <router-link  to="/">home</router-link></li>
+                <li v-for="destination in destinations" :key="destination.name" class="links">
+                    <router-link :to="{
+                        name:'destinationdetails',
+                        params:{id:destination.id}
+                        }">
+                        {{destination.name}}
+                        </router-link>
+                </li>
+            
+        </ul>
+    </nav>
+</template>
+
+<script>
+import store from '@/store.js'
+export default {
+    data(){
+        return{
+            destinationid:this.$route.params.id,
+            destinations:store.destinations
+        }
+    }
+}
+</script>
+<style scoped>
+#nav{
+    display: flex;
+    background-color: rgb(207, 199, 245);
+}
+
+
+#nav a {
+  color: #2c3e50;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: #dd402b;
+}
+.nav-links{
+    display: flex;
+}
+.links{
+    padding-right: 20px;
+    list-style: none;
+}
+.links:hover{
+    text-decoration: underline;
+}
+.logo{
+    font-size: 25px;
+    color: #5f1159;
+    font-weight: bold;
+}
+</style>
